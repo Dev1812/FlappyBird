b8c3ae0ba737801510531b57a2a41833d1d5159b
Timur <timka_issaev@mail.ru>
2020-01-08 18:57:47 +0300

    added paths to images

--- js/game.js
+++ js/game.js
@@ -2,4 +2,10 @@ var bird = new Image();
 var bg = new Image(); 
 var fg = new Image(); 
 var pipeUp = new Image(); 
-var pipeBottom = new Image(); 
+var pipeBottom = new Image();
+
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
