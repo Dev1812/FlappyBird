830cdd92ff4690447b77e48f9b16326cf4d86f6a
Timur <timka_issaev@mail.ru>
2020-01-08 19:24:09 +0300

    game.js was improved

--- js/game.js
+++ js/game.js
@@ -1,7 +1,6 @@
 function ge(el) {
   return (typeof el == 'string' || typeof el == 'number') ? document.getElementById(el) : el;
 }
-
 function addEvent(el, event_type, handler) {
   el = ge(el);
   if(window.addEventListener) {
@@ -13,41 +12,37 @@ function addEvent(el, event_type, handler) {
   }
 }
 
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
 
-  var bird = new Image();
+var bird = new Image();
+var bg = new Image(); 
+var fg = new Image();
 
-  var bg = new Image(); 
+var pipeUp = new Image();
+var pipeBottom = new Image();
 
-  var fg = new Image(); 
+/**
+ * Создание блоков
+ */ 
+var pipe = [];
+pipe[0] = {
+  x : cvs.width,
+  y : 0
+}
 
-  var pipeUp = new Image();
+/** 
+ * Баллы
+ */ 
+var score = 0;
 
-  var pipeBottom = new Image();
-
-  /**
-   * Создание блоков
-   */ 
-  var pipe = [];
-
-  pipe[0] = {
-    x : cvs.width,
-    y : 0
-  }
-
-  /** 
-   * Баллы
-   */ 
-  var score = 0;
-  /** 
-   * Позиция птицы
-   */ 
-  var xPos = 10;
-
-  var yPos = 150;
-
-  var grav = 1.5;
-
-  var gap = 90;
+/**
+ * Позиция птицы
+ */
+var xPos = 10;
+var yPos = 150;
+var grav = 1.5;
+var gap = 90;
 
 bird.src = "img/bird.png";
 bg.src = "img/bg.png";
@@ -55,15 +50,10 @@ fg.src = "img/fg.png";
 pipeUp.src = "img/pipeUp.png";
 pipeBottom.src = "img/pipeBottom.png";
 
-
-  setTimeout(draw(), 10);
-
-
 addEvent(document, 'keydown', moveUp);
 
 function moveUp() {
  yPos -= 25;
- fly.play();
 }
 
 function draw() {
